2f569a64f3ec84515f9ee35e2f56fd52974b82fb
smk-141510179 <141510179@smkassalaambandung.sch.id>
2017-02-23 14:55:45 +0700

    :hari-ketiga

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-success">
+                <div class="panel-heading">^^Selamat Datang^^</div>
+
+                <div class="panel-body">
+                    ---Di Aplikasi Penggajian---
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/tunjanganp/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+@section('content')
+<h1>Tunjangan Pegawai</h1>
+<a  href="{{url('tunjanganp/create')}}" class="btn btn-success  form-control">Tambah</a>
+	<table border="1" class="table table-striped table-border table-hover">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Kode Kategori Tunjangan</th>
+				<th>Nama Pegawai</th>
+				<th colspan="1"><center>Action</center></th>
+			</tr>
+		</thead>
+		@php $no=1; @endphp
+		<tbody>
+			@foreach($tunjanganp as $data)
+			<tr>
+				<td>{{$no++}}</td>
+				<td>{{$data->kode_tunjangan_id}}</td>
+				<td>{{$data->pegawai->user->name}}</td>
+				
+					
+				</td>
+				<td>
+					{!! Form::open(['method'=>'DELETE','route'=>['tunjanganp.destroy',$data->id]]) !!}
+					{!! Form::submit('Delete',['class'=>'btn btn-danger']) !!}
+					{!! Form::close() !!}
+				</td>
+			</tr>
+			@endforeach
+		</tbody>
+	</table>
+	
+
+@endsection
